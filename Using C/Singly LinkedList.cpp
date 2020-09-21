e4abd50f7959395348511c7158a732234cd09bcc
Sanzid olioul <sanzid94@gmail.com>
2020-09-21 08:34:45 +0600

    Some functionality added

--- Using C/Singly LinkedList.cpp
+++ Using C/Singly LinkedList.cpp
@@ -5,20 +5,55 @@ class LinkedList{
         int data;
         struct Node *next;
         };
+        struct Node *head = NULL,*newNode,*tail,*sLast;
     public:
-        struct Node *head = NULL,*newNode,*temp;
+        struct Node *travel;
+        LinkedList(){
+            travel=(struct Node*)malloc(sizeof(struct Node));
+        }
+        //push_back implementation
         void push_back(int n)
         {
             newNode = (struct Node*)malloc(sizeof(struct Node));
             if(head== NULL){
-                head = temp = newNode;
+                head = newNode;
                 head->data = n;
+                head->next =NULL;
+                travel = head;
             }
             else{
-                temp->next = newNode;
-                temp->data = n;
-                temp = newNode;
+                if(head->next ==NULL){
+                    tail = newNode;
+                    tail->data = n;
+                    head->next = tail;
+                    tail->next =NULL;
+                }else{
+                    //struct Node *temp;
+                    tail->next = newNode;
+                    tail = newNode;
+                    tail->data = n;
+                    tail->next = NULL;
+                }
             }
+        }
+        //push_front implementation
+        void push_front(int n)
+        {
+            struct Node *temp;
+            newNode = (struct Node*)malloc(sizeof(struct Node));
+            temp = newNode;
+            temp->data = n;
+            temp->next = head;
+            head = temp;
+        }
+
+        int front()
+        {
+            return head->data;
+        }
+        int back()
+        {
+            return tail->data;
         }
 };
 
@@ -26,6 +61,7 @@ class LinkedList{
 
 int main()
 {
+    int first,last;
     LinkedList lst,lst2;
     lst.push_back(5);
     lst.push_back(6);
@@ -40,20 +76,26 @@ int main()
     lst2.push_back(13);
     lst2.push_back(14);
     lst2.push_back(15);
+
+    lst2.push_front(100);
+    lst2.push_front(110);
+    lst2.push_front(111);
     lst2.push_back(16);
-    lst.temp = lst.head;
-    while(lst.temp->next!= NULL){
-        std::cout<<lst.temp->data<<std::endl;
-        lst.temp = lst.temp->next;
+    lst2.push_front(102);
+    lst2.push_front(113);
+    lst2.push_front(114);
+    while(lst.travel){
+        std::cout<<lst.travel->data<<std::endl;
+        lst.travel = lst.travel->next;
     }
 
 
     std::cout<<std::endl;
-    lst2.temp = lst2.head;
-    while(lst2.temp->next!= NULL){
-        std::cout<<lst2.temp->data<<std::endl;
-        lst2.temp = lst2.temp->next;
+    while(lst2.travel){
+        std::cout<<lst2.travel->data<<std::endl;
+        lst2.travel = lst2.travel->next;
     }
+    std::cout<<std::endl<<lst.front()<<std::endl<<lst.back()<<std::endl;
 
     return 0;
 }
